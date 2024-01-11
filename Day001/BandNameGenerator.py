6201c29d92d32a54a90004e2140e4b4c785018ec
Joseph Pereira <joseph.carlosx@gmail.com>
2024-01-11 18:01:05 -0400

    feat: func Band Name Generator

---
+++ Day001/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("This is the Band' Name Generator");
+city = input("Please, type the name of your City.\n");
+pet = input("Plese, type the name of your pet.\n");
+print(f"The name of your band is: {city} {pet}");
